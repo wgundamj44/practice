6d1062e4f2f4dc6f07138d936aaa0e4774f16ca8
wgundamj44 <wgundamj44@gmail.com>
2015-06-09 18:50:26 +0900

    Create sort-colors.cc

---
+++ sort-colors.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) { 
+        int i = 0, len = nums.size();
+        // keep swapping, so that 2s are to the end, and first index of 1s are behind 0.
+        int f1 = -1, f2 = len - 1;
+        while(i <= f2) {
+            if (nums[i] == 0) {
+                if (f1 != -1 && i > f1) {
+                    swap(nums[f1], nums[i]);
+                    f1++;
+                } 
+                i++;
+            }
+            else if (nums[i] == 1) {
+                if (f1 == -1) {
+                    f1 = i;
+                }
+                i++;
+            }
+            else if (nums[i] == 2) {
+                if (i != f2) {
+                    swap(nums[i], nums[f2]);
+                    f2--;
+                } else {
+                    break;   
+                }
+            }
+        }
+    }
+};
